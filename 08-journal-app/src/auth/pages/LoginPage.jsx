215db216e6c1ce093d20bc278593cc9d7634a69b
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-02-27 10:48:11 -0300

    add styling loginPage

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,5 +1,70 @@
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 export const LoginPage = () => {
-  return <div>LoginPagin</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          Login
+        </Typography>
+
+        <form action="" method="get">
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Email"
+                type="email"
+                placeholder="correo@google.com"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Password"
+                type="password"
+                placeholder="correo@google.com"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid container spacing={2} sx={{ md: 2, mt: 1 }}>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row" justifyContent="end" sx={{ mt: 1 }}>
+              <Link component={RouterLink} color="inherit" to="/auth/signup">
+                Crear una cuenta
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
 };
--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,6 +1,12 @@
+import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import React from "react";
 
 export const JournalPage = () => {
-  return <Typography variant={"h2"}>JournalPage</Typography>;
+  return (
+    <>
+      <MailOutline />
+      <Typography variant={"h2"}>JournalPage</Typography>
+    </>
+  );
 };
