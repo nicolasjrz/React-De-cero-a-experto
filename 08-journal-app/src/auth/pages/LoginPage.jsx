f8c66451dad6c3949c40c3da2db957ec47f5315c
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-02-27 12:06:21 -0300

    layout and styling signup

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -2,69 +2,52 @@ import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
-    >
-      <Grid
-        item
-        className="box-shadow"
-        xs={3}
-        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant="h5" sx={{ mb: 1 }}>
-          Login
-        </Typography>
+    <AuthLayout title="Login">
+      <form action="" method="get">
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
 
-        <form action="" method="get">
-          <Grid container>
-            <Grid item xs={12} sx={{ mt: 2 }}>
-              <TextField
-                label="Email"
-                type="email"
-                placeholder="correo@google.com"
-                fullWidth
-              />
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Insert password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ md: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
             </Grid>
-
-            <Grid item xs={12} sx={{ mt: 2 }}>
-              <TextField
-                label="Password"
-                type="password"
-                placeholder="correo@google.com"
-                fullWidth
-              />
-            </Grid>
-
-            <Grid container spacing={2} sx={{ md: 2, mt: 1 }}>
-              <Grid item xs={12} sm={6}>
-                <Button variant="contained" fullWidth>
-                  Login
-                </Button>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Button variant="contained" fullWidth>
-                  <Google />
-                  <Typography sx={{ ml: 1 }}>Google</Typography>
-                </Button>
-              </Grid>
-            </Grid>
-
-            <Grid container direction="row" justifyContent="end" sx={{ mt: 1 }}>
-              <Link component={RouterLink} color="inherit" to="/auth/signup">
-                Crear una cuenta
-              </Link>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
             </Grid>
           </Grid>
-        </form>
-      </Grid>
-    </Grid>
+
+          <Grid container direction="row" justifyContent="end" sx={{ mt: 1 }}>
+            <Link component={RouterLink} color="inherit" to="/auth/signup">
+              Create Account
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   );
 };
--- 08-journal-app/src/auth/pages/SignupPage.jsx
+++ 08-journal-app/src/auth/pages/SignupPage.jsx
@@ -1,5 +1,62 @@
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <AuthLayout title="Signup">
+      <form action="" method="get">
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Full name"
+              type="text"
+              placeholder="John Doe"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Insert password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ md: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Create Account
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end" sx={{ mt: 1 }}>
+            <Typography>You already have an account?</Typography>
+            <Link
+              component={RouterLink}
+              color="inherit"
+              to="/auth/signup"
+              sx={{ ml: 1 }}
+            >
+              Login
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
