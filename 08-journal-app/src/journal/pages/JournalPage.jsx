f4dac915025862076f3fb6b1945e72ca337cebc6
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-06 15:29:39 -0300

    add journal page

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import React from "react";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView } from "../view/NoteView";
@@ -6,9 +8,23 @@ import { NothingSelectedView } from "../view/NothingSelectedView";
 export const JournalPage = () => {
   return (
     <JournalLayout>
+      <NothingSelectedView />
       {/** <NothingSelectedView />*/}
-      {/** nothinSeleted*/}
-      <NoteView />
+      {/** <NoteView />*/}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
