35497c8dbc960e53f10dc93154c2550e8334cbbd
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-18 16:03:37 -0300

    add files in proyect and install react-router

---
+++ 10-calendar/src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { CalendarPage } from "../calendar/pages/CalendarPage";
+
+export const AppRouter = () => {
+  const auhtStatus = "not-auth";
+
+  return (
+    <Routes>
+      {auhtStatus === "not-auth" ? (
+        <Route path="/auth/*" element={<LoginPage />} />
+      ) : (
+        <Route path="/*" element={<CalendarPage />} />
+      )}
+
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  );
+};
