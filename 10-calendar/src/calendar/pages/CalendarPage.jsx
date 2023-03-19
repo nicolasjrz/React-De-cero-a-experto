8338b4e72e9e49b022d1f2262c55e0908a874bd8
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-19 16:56:10 -0300

    add dependency react-big-calendar

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import { Navbar } from "./components/Navbar";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import enUS from "date-fns/locale/en-US";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "nota test",
+    notes: "prueba de nota",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </>
   );
 };
