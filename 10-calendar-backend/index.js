82944bf4f85bafaf5de0974043aeb28f7817a3c5
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-21 11:11:01 -0300

    add dotenv

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
+//require("dotenv").config({ path: "./config.env" });
+
+require("dotenv").config();
 
 // crear el servidor de express
 const app = express();
 
 //rutas
 
-app.get("/", (req, res) => {
-  console.log("se requiere el /");
-  res.json({ ok: true });
-});
+/// directorio publicoPORT=4000
+
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   console.log("se requiere el /");
+//   res.json({ ok: true });
+// });
 
 /// escuchar peticion
-app.listen(4000, () => {
-  console.log("servidor corriendo en el puerto 4000");
+app.listen(process.env.PORT, () => {
+  console.log(`servidor corriendo en el puerto ${process.env.PORT}`);
 });
