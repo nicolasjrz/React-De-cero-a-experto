0e4ade9994d05f6cabb22b260c0875d10295be41
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-21 10:33:46 -0300

    install express

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,16 @@
-console.log("hola mundo");
+const express = require("express");
+
+// crear el servidor de express
+const app = express();
+
+//rutas
+
+app.get("/", (req, res) => {
+  console.log("se requiere el /");
+  res.json({ ok: true });
+});
+
+/// escuchar peticion
+app.listen(4000, () => {
+  console.log("servidor corriendo en el puerto 4000");
+});
