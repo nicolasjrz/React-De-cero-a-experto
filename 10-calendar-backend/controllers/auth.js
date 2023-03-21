3f83b12dd9b647a35a13907903362cfa55e45808
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-21 12:36:37 -0300

    read req.body

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,13 +1,15 @@
 const { response } = require("express");
 
 const crearUsuario = (req, res = response) => {
-  console.log("se requiere el /");
-  res.json({ ok: true, msg: "register" });
+  const { name, email, password } = req.body;
+
+  res.json({ ok: true, msg: "register", name, email, password });
 };
 
 const loginUsuario = (req, res = response) => {
-  console.log("se requiere el /");
-  res.json({ ok: true, msg: "login" });
+  const { email, password } = req.body;
+
+  res.json({ ok: true, msg: "login", email, password });
 };
 
 const revalidarToken = (req, res = response) => {
--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -6,13 +6,16 @@ require("dotenv").config();
 // crear el servidor de express
 const app = express();
 
+// lectura y parseo del body
+
+app.use(express.json());
+
 //rutas
 
-/// directorio publicoPORT=4000
-
-app.use(express.static("public"));
-
 app.use("/api/auth", require("./routes/auth"));
+
+/// directorio publicoPORT=4000
+app.use(express.static("public"));
 
 /// escuchar peticion
 app.listen(process.env.PORT, () => {
