ace3203655f4a7a5498d29b7f388e527bbd346db
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-21 16:35:42 -0300

    git ignore

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,15 +1,34 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
 
-  res.json({ ok: true, msg: "register", name, email, password });
+  /// manejo de errores
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(201).json({ ok: true, msg: "register", name, email, password });
 };
 
 const loginUsuario = (req, res = response) => {
   const { email, password } = req.body;
 
-  res.json({ ok: true, msg: "login", email, password });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(401).json({ ok: true, msg: "login", email, password });
 };
 
 const revalidarToken = (req, res = response) => {
--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 const {
   crearUsuario,
   loginUsuario,
@@ -6,9 +7,28 @@ const {
 } = require("../controllers/auth");
 const router = express.Router();
 
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    check("name", "el nombre es obligatirio").not().isEmpty(),
+    check("email", "el email es obligatirio").isEmail(),
+    check("password", "el password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  crearUsuario
+);
 
-router.post("/", loginUsuario);
+router.post(
+  "/",
+  [
+    check("email", "el email es obligatirio").isEmail(),
+    check("password", "el password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  loginUsuario
+);
 
 router.get("/renew", revalidarToken);
 
