8e60ee3439a138e2b3aa5b1efbda52677c85bcf0
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-22 00:43:04 -0300

    add middlewares

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,18 +1,7 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
-
-  /// manejo de errores
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.status(201).json({ ok: true, msg: "register", name, email, password });
 };
@@ -20,19 +9,10 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
   const { email, password } = req.body;
 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
-
   res.status(401).json({ ok: true, msg: "login", email, password });
 };
 
 const revalidarToken = (req, res = response) => {
-  console.log("se requiere el /");
   res.json({ ok: true, msg: "renew" });
 };
 
--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
 const {
   crearUsuario,
   loginUsuario,
   revalidarToken,
 } = require("../controllers/auth");
+
 const router = express.Router();
 
 router.post(
@@ -15,6 +17,7 @@ router.post(
     check("password", "el password debe de ser de 6 caracteres").isLength({
       min: 6,
     }),
+    validarCampos,
   ],
   crearUsuario
 );
@@ -26,6 +29,7 @@ router.post(
     check("password", "el password debe de ser de 6 caracteres").isLength({
       min: 6,
     }),
+    validarCampos,
   ],
   loginUsuario
 );
@@ -33,3 +37,7 @@ router.post(
 router.get("/renew", revalidarToken);
 
 module.exports = router;
+
+// check('password',
+// `La contraseña debe tener mínimo 8 caracteres.Debe contener una letra minúscula.Debe contener una letra mayúscula.Debe contener un carácter especial.Debe contener mínimo un número.`
+// ).isLength({ min: 8 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
