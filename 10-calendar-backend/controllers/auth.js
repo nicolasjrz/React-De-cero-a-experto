52fe489d3c0457a590ea6343cd7ddeda7bfd8fd7
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-22 12:34:15 -0300

    add user in database

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,9 +1,23 @@
 const { response } = require("express");
 
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
+const Usuario = require("../models/User");
 
-  res.status(201).json({ ok: true, msg: "register", name, email, password });
+const crearUsuario = async (req, res = response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const usuario = new Usuario(req.body);
+
+    await usuario.save();
+
+    res.status(201).json({ ok: true, msg: "register", name, email, password });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
