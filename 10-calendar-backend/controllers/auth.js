228ceae6ce1b7deca17bfda49624e708833af7dc
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-22 14:18:27 -0300

    validation password mongoose

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -3,9 +3,8 @@ const bcrypt = require("bcryptjs");
 const Usuario = require("../models/User");
 
 const crearUsuario = async (req, res = response) => {
+  const { name, email, password } = req.body;
   try {
-    const { name, email, password } = req.body;
-
     let usuario = await Usuario.findOne({ email });
 
     if (usuario) {
@@ -33,8 +32,38 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
+  try {
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario y password invalidos.",
+      });
+    }
+
+    //confinar los passwords
+
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+
+    /// generar nuestro JWT
+
+    res.json({ ok: true, uid: usuario._id, name: usuario.name });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 
   res.status(401).json({ ok: true, msg: "login", email, password });
 };
