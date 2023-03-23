832ef0d75e78229f06255aee0b165962c1ff2134
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-23 19:00:31 -0300

    upload event in mongoDB

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -36,11 +36,39 @@ const getEvents = async (req, res = response) => {
   }
 };
 
-const uploadEvent = (req, res = response) => {
+const uploadEvent = async (req, res = response) => {
+  const eventID = req.params.id;
+  const uid = req.uid;
   try {
+    const evento = await Event.findById(eventID);
+
+    if (!evento)
+      return res.status(404).json({
+        ok: false,
+        msg: "evento no existe por ese id",
+      });
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "no tiene permisos para editar el evento",
+      });
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid,
+    };
+
+    const eventUpload = await Event.findByIdAndUpdate(eventID, newEvent, {
+      new: true,
+    });
+
     res.status(200).json({
       ok: true,
       msg: " evento actualizado correctamente",
+      eventID,
+      evento: eventUpload,
     });
   } catch (error) {
     return res.status(500).json({
