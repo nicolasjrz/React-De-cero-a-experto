9eb364dcd4beeb2f5b36288b9d96458df1a94062
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-03-28 18:11:36 -0300

    add .env and funtion

--- 10-calendar/src/router/AppRouter.jsx
+++ 10-calendar/src/router/AppRouter.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { CalendarPage } from "../calendar/pages/CalendarPage";
+import { getEnvVariable } from "../helpers/getEnvVariable";
 
 export const AppRouter = () => {
   const auhtStatus = "auth";
+
+  //console.log(getEnvVariable());
 
   return (
     <Routes>
